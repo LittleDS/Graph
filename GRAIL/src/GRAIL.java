9f6e2c5efdb0d43275ed0ce1b629967ab48c76ad
LittleDS <yanglei0073@gmail.com>
2012-08-13 23:43:24 -0400

    GRAIL framework has been built

---
+++ GRAIL/src/GRAIL.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright reserved by Lei Yang @ Case Western Reserve University
+ * August 13th, 2012
+ */
+public class GRAIL {
+
+	public static void main(String[] args) {		
+		
+	}
+
+	/**
+	 * Given a graph file, this method build the interval labeling index
+	 * using the post-order traversal 
+	 */
+	public static void Encoding() {
+				
+	}
+	
+	
+
+}
