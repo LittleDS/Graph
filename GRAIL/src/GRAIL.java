859b31cb25b4e7548e1070298b4946fafc08d384
LittleDS <yanglei0073@gmail.com>
2012-08-15 11:51:29 -0400

    Random feature has been added.  Check function is complete

--- GRAIL/src/GRAIL.java
+++ GRAIL/src/GRAIL.java
@@ -1,5 +1,8 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 /**
  * Copyright reserved by Lei Yang @ Case Western Reserve University
@@ -7,16 +10,15 @@ import java.util.*;
  */
 public class GRAIL {
 
-	public static void main(String[] args) throws FileNotFoundException {		
+	public static void main(String[] args) throws IOException {		
 		GRAIL tGrail = new GRAIL();
 		tGrail.Encoding("graphexample.txt", 2);
-		for (Integer i: tGrail.intervalLabel.keySet()) {
-			System.out.print(i);
-			for (Integer j: tGrail.intervalLabel.get(i))
-				System.out.print(" " + j);
-			System.out.println();
-		}
-		
+//		for (Integer i: tGrail.intervalLabel.keySet()) {
+//			System.out.print(i);
+//			for (Integer j: tGrail.intervalLabel.get(i))
+//				System.out.print(" " + j);
+//			System.out.println();
+//		}
 	}
 
 	//Since the attributes of each vertex don't have any contribution to the labeling, we can split them into a separate structure
@@ -32,9 +34,9 @@ public class GRAIL {
 	 * Given a graph file, this method build the interval labeling index
 	 * using the post-order traversal 
 	 * @param D is the number of different interval labelings  
-	 * @throws FileNotFoundException 
+	 * @throws IOException 
 	 */
-	public void Encoding(String fileName, int D) throws FileNotFoundException {
+	public void Encoding(String fileName, int D) throws IOException {
 		File tFile = new File(fileName);
 		Scanner tScanner = new Scanner(tFile);
 		
@@ -77,6 +79,7 @@ public class GRAIL {
 			for (Integer j : neighbors.get(i))
 				inDegree[j]++;
 		}
+		
 		//The roots are those vertices with 0 in-degree
 		LinkedList<Integer> roots = new LinkedList<Integer>();
 		for (int i = 0; i < totalVertices; i++)
@@ -99,21 +102,30 @@ public class GRAIL {
 			for (int j = 0; j < totalVertices; j++) visited[j] = false;
 
 			//randomly shuffle the roots
-			java.util.Collections.shuffle(roots);
+			Collections.shuffle(roots);
 			
-			tempLabel = new int[totalVertices][2];
+			//Initialize the array for current labels
+			tempLabel = new int[totalVertices][2];			
 			for (int j = 0; j < totalVertices; j++) {
 				tempLabel[j][0] = -1;
 				tempLabel[j][1] = -1;
 			}
+			
+			//Since there could be multiple roots in the graph
+			//we need to label them all
 			for (Integer j : roots) {
 				DFSLabel(j);
+				
+				//Assign the current label to the final result
 				for (int k = 0; k < totalVertices; k++) {
 					intervalLabel.get(k).add(tempLabel[k][0]);
 					intervalLabel.get(k).add(tempLabel[k][1]);
 				}
 			}			
 		}
+		
+		//Output the index to a file
+		outputToFile("index.txt");
 	}
 
 	
@@ -133,9 +145,12 @@ public class GRAIL {
 		if (visited[x]) return;
 		visited[x] = true;
 		
+		//Visit the children randomly
+		Collections.shuffle(neighbors.get(x));
 		for (Integer i : neighbors.get(x))
 			DFSLabel(i);
 		
+		//Get the minimum rank value from the children of current vertex
 		int rc = Integer.MAX_VALUE;		
 		for (Integer i : neighbors.get(x))
 			if (tempLabel[i][0] != -1) {
@@ -161,4 +176,73 @@ public class GRAIL {
 		else
 			return b;
 	}
+	
+	/**
+	 * Output the index to a file for future use
+	 * @param fileName
+	 * @throws IOException 
+	 */
+	public void outputToFile(String fileName) throws IOException {
+		FileWriter fstream = new FileWriter(fileName);
+		BufferedWriter out = new BufferedWriter(fstream);
+		for (int i = 0; i < intervalLabel.keySet().size(); i++) {
+			String t = i + "";
+			for (Integer j : intervalLabel.get(i))
+				t += "," + j;
+			out.write(t + "\r\n");
+		}
+		out.close();		
+	}
+	
+	/**
+	 * Load the index from the file
+	 * @param fileName
+	 * @throws FileNotFoundException
+	 */
+	public void loadIndexFromFile(String fileName) throws FileNotFoundException {
+		File tFile = new File(fileName);
+		Scanner tScanner = new Scanner(tFile);
+		//Just in case
+		intervalLabel.clear();
+		
+		while (tScanner.hasNext()) {
+			//Read the next line
+			String currentLine = tScanner.nextLine();
+
+			//Split the integers
+			String[] strings = currentLine.split(",");
+			
+			//Fetch the vertex ID
+			int ID = Integer.parseInt(strings[0]);
+			
+			//Initialize the intervalLabel
+			intervalLabel.put(ID, new LinkedList<Integer>());
+			
+			//Insert the labels into the structure
+			for (int i = 1; i < strings.length; i++) {
+				intervalLabel.get(ID).add(Integer.parseInt(strings[i]));
+			}
+			
+		}
+		
+	}
+
+	/**
+	 * This method check whether the interval label of y is contained in x
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public boolean CheckContainment(int x, int y) {
+		List<Integer> xL = intervalLabel.get(x);
+		List<Integer> yL = intervalLabel.get(y);
+		//Check the intervals one by one
+		for (int i = 0; i < xL.size() / 2; i+=2) {
+			//  If the only condition doesn't hold, we can return false directly
+			if (!(xL.get(i) < yL.get(i) && xL.get(i + 1) > yL.get(i + 1)))
+				return false;
+		}
+		return true;				
+	}
+	
 }
