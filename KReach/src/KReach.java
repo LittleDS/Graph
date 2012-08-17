63fed98cf261bc0b036570901bdb50ee46984f3c
LittleDS <yanglei0073@gmail.com>
2012-08-17 17:58:59 -0400

    Vertex Cover algorithm has been finished.  Brieftly Test. Need further Test.

--- KReach/src/KReach.java
+++ KReach/src/KReach.java
@@ -1,12 +1,13 @@
-import java.awt.datatransfer.StringSelection;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  * Copyright reserved by Lei Yang @ Case Western Reserve University
@@ -17,16 +18,27 @@ public class KReach {
 	HashMap<Integer, List<String>> attributes = new HashMap<Integer, List<String>>();
 
 	//The adjacency list of each vertex
-	HashMap<Integer, List<Integer>> neighbors = new HashMap<Integer, List<Integer>>();
-
+	HashMap<Integer, List<Integer>> children = new HashMap<Integer, List<Integer>>();
+	HashMap<Integer, List<Integer>> parents = new HashMap<Integer, List<Integer>>();
+	int totalEdges = 0;
+	
+	
 	public static void main(String[] args) throws FileNotFoundException {
 		KReach kr = new KReach();
 		kr.Encode("graphexample.txt");
 	}
-	
+
+	/**
+	 * 
+	 * @param fileName
+	 * @throws FileNotFoundException
+	 */
 	public void Encode(String fileName) throws FileNotFoundException {
+		//The input file
 		File tFile = new File(fileName);
+		
 		Scanner tScanner = new Scanner(tFile);
+		
 		//Read the original graph file and build the graph in the main memory
 		while (tScanner.hasNext()) {
 			//Read the graph file
@@ -47,52 +59,187 @@ public class KReach {
 				attributes.get(ID).add(strings[i]);
 			}
 			
-			//Process the neighbors and also transform the graph into a undirected one
+			//Process the neighbors
 			strings = neighborLine.split(",");
-			if (!neighbors.containsKey(ID))
-				neighbors.put(ID, new LinkedList<Integer>());
+			if (!children.containsKey(ID))
+				children.put(ID, new LinkedList<Integer>());
 			for (int i = 0; i < strings.length; i++) {
 				int tN = Integer.parseInt(strings[i]);
 				//If the neighbor ID is equal to -1, it means the current vertex doesn't have a neighbor
 				if (tN != -1) {
-					neighbors.get(ID).add(tN);
-					if (!neighbors.containsKey(tN))
-						neighbors.put(tN, new LinkedList<Integer>());
-					neighbors.get(tN).add(ID);						
+					children.get(ID).add(tN);
+					//Calculate the total number of edges
+					totalEdges++;
+					
+					//The parents of each node is also record
+					if (!parents.containsKey(tN))
+						parents.put(tN, new LinkedList<Integer>());
+					parents.get(tN).add(ID);						
 				}				
 			}			
 		}
 		tScanner.close();		
 		
-		GreedyVertexCover();
+		//Calculat the vertex cover using the simplest method
+		CalculateVertexCoverGreedy();
+		
+		for (Integer i : VC)
+			System.out.println(i);
 	}
 
+	//The vertex cover
+	Set<Integer> VC = new HashSet<Integer>();
 	
-
 	/**
-	 * Obtain the vertex cover using the greedy method
+	 * Obtain the vertex cover using the simplest method
 	 */
-	public void GreedyVertexCover() {
-		//Calculate the degree of each vertex
-		int totalVertices = attributes.keySet().size();
+	public void CalculateVertexCoverSimple() {
+		VC.clear();
 		
-		int numberofEdge = 0;
+		Random randomGenerator = new Random();
+		//Get all the vertices
+		LinkedList<Integer> vertices = new LinkedList<Integer>(children.keySet());
 		
-		int[] degree = new int[totalVertices];
+		//Mark if one vertex is used
+		boolean[] used = new boolean[vertices.size()];
 		
-		Integer[] index = new Integer[totalVertices];
+		//Randomly shuffle the vertices and it's equal to randomly pick up one vertex from the set
+		Collections.shuffle(vertices);
 		
-		for (int i = 0; i < totalVertices; i++) {
-			index[i] = i;
-			//out-degree
-			degree[i] += neighbors.get(i).size();
+		//The algorithm is straightforward.  We first choose a vertex and from the children of this vertex we choose the other one
+		for (Integer i : vertices) {
+			//If this vertex is used before, we can skip it
+			if (used[i]) continue;
 			
-			numberofEdge += degree[i];
+			//Mark it as used
+			used[i] = true;
 			
-			for (Integer j : neighbors.get(i))
-				//in-degree
-				degree[j]++;
-		}
+			//Get the children of the first vertex
+			List<Integer> edges = children.get(i);
+			
+			//In case the vertex doesn't have any child, we skip it
+			if (edges.size() == 0) continue;
+			
+			//Randomly choose one of its children
+			Integer j = edges.get(randomGenerator.nextInt(edges.size()));
+			used[j] = true;
+			
+			//Add the two vertices into the set
+			VC.add(i);
+			VC.add(j);
+			
+			//Remove the corresponding edges
+			//First remove the children of these two vertices
+			totalEdges -= children.get(i).size();
+			children.remove(i);
+			
+			totalEdges -= children.get(j).size();			
+			children.remove(j);
+			
+			//For every parent of the vertex, we remove the vertex from their list one by one
+			if (parents.containsKey(i)) {				
+				for (Integer k : parents.get(i)) {
+					if (children.containsKey(k)) {
+						children.get(k).remove(i);					
+						totalEdges--;
+						if (children.get(k).size() == 0)
+							used[k] = true;
+					}
+				}
+			}
+			if (parents.containsKey(j)) {
+				for (Integer k : parents.get(j)) {
+					if (children.containsKey(k)) {
+						children.get(k).remove(j);
+						totalEdges--;
+						if (children.get(k).size() == 0)
+							used[k]= true;
+					}
+				}
+			}
+			
+			//If the total number of edges left is zero, we're done
+			if (totalEdges == 0)
+				break;
+		}		
+	}
+	
+	public void CalculateVertexCoverGreedy() {
+		VC.clear();
 
+		//The purpose of this structure is to reduce the size of the candidate vertices set
+		//Since each time we remove at least one vertex from this set
+		//The performance should be increased by at least a factor of 2
+		HashSet<Integer> candidateVertices = new HashSet<Integer>(children.keySet());
+		
+		//First calculate the degree of each vertex
+		int[] degree = new int[children.keySet().size()];	
+		for (Integer i: children.keySet()) {
+			if (children.containsKey(i))
+				degree[i] += children.get(i).size();
+			if (parents.containsKey(i))			
+				degree[i] += parents.get(i).size();
+		}
+		
+		//Iteratively choose the vertex cover
+		while (totalEdges > 0) {
+		
+			//Choose the one with maximum degree
+			int maximumDegree = 0;
+			Integer chosenVertex = null;
+			for (Integer i : candidateVertices) {
+				if (degree[i] > maximumDegree) {
+					maximumDegree = degree[i];
+					chosenVertex = i; 
+				}
+			}
+			
+			//Add the chosen vertex into the vertex cover set
+			VC.add(chosenVertex);
+			
+			//Modify the degree of that vertex to 0, because we will remove all the edges attaching to it
+			degree[chosenVertex] = 0;			
+			
+			//Also remove that vertex from the candidate sets
+			candidateVertices.remove(chosenVertex);
+			
+			//Remove all the children edges and also update the degree of those children
+			if (children.containsKey(chosenVertex)) {
+				//Update the total number of edges first
+				totalEdges -= children.get(chosenVertex).size();
+				
+				//Update the degree of those children
+				for (Integer j : children.get(chosenVertex)) {
+					degree[j]--;
+					if (degree[j] == 0)
+						candidateVertices.remove(j);
+				}
+				
+				//Remove all those children edges
+				children.remove(chosenVertex);				
+			}
+			
+			//Remove all the parents edges and also update the degree
+			if (parents.containsKey(chosenVertex)) {
+				
+				//For each parent
+				for (Integer j : parents.get(chosenVertex))
+				
+					//Check whether this parent has been processed before
+					if (children.containsKey(j) ) {
+					
+						//From the children list, remove that chosen vertex
+						children.get(j).remove(chosenVertex);
+						
+						//Also update the degree of that parent and the total number of edges0
+						degree[j]--; 						
+						totalEdges--;
+						
+						//If the total degree of that parent becomes 0, then we can remove it from the candidate set
+						if (degree[j] == 0)
+							candidateVertices.remove(j);							
+					}
+			}			
+		}
 	}
 }
