696236b99f600668ba38a23661407958bfced9f1
LittleDS <yanglei0073@gmail.com>
2012-11-21 13:21:55 -0500

    Refactor the directory

--- GraphParser/src/GraphParser.java
+++ GraphParser/src/GraphParser.java
@@ -14,8 +14,8 @@ import java.util.Scanner;
 public class GraphParser {
 	public static void main(String[] args) throws IOException {
 		GraphParser g = new GraphParser();
-		g.Parser("p2p.txt");
-		g.output("p2pData.txt");
+		g.Parser("Amazon0302.txt");
+		g.output("Amazon.txt");
 		System.out.println("Done.");
 	}
 	
